e4f91df4f113e50bdc4a6694ff54a7f4705a45bb
Sierra Ponce <sierrarayne0220@gmail.com>
2019-09-14 03:01:04 +0000

    adding a model

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,11 +1,7 @@
 class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first 
-  end  
-
-  def new
-    @quote = Quote.new 
-  end
+  end 
 
   def create
     @quote = Quote.create(quote_pams)
